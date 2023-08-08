7e405ff329db4ec3e1673fffaaf2226c8fb19ebe
Sergio Peralta <seperalta88@gmail.com>
2023-08-08 03:26:32 -0300

    Added games table with foreign key constraint to authors table.

--- schema.sql
+++ schema.sql
@@ -5,3 +5,15 @@ CREATE TABLE authors(
     last_name VARCHAR(50),
     PRIMARY KEY(id)
 );
+
+-- games
+CREATE TABLE games(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN,
+    multiplayer BOOLEAN,
+    last_played_at DATE NOT NULL,
+    games_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_authors FOREIGN KEY(games_id) REFERENCES authors(id)
+);
