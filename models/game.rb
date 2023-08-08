c13c022660699e532e853a0b4ad2203126b2513a
Sergio Peralta <seperalta88@gmail.com>
2023-08-08 02:33:48 -0300

    Add game methods test

--- models/game.rb
+++ models/game.rb
@@ -1,3 +1,6 @@
+require_relative '../item'
+
+
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :id
 
@@ -10,6 +13,6 @@ class Game < Item
   private
 
   def can_be_archived?
-    super && (Date.today - @last_played_at).to_i >= 365 * 2
+    super && Time.new.year - last_played_at > 2
   end
 end
