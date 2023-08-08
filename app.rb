6b078958aa069e45e053c4a4c3e0788a2256546e
Luis Carlos Valera <lc26cr7@gmail.com>
2023-08-08 12:10:58 -0400

    Update app.rb to fix linters errors

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'message_outputs'
 require './models/music_album'
+require './models/genre'
 
 class App
   include MessageOutputs
@@ -21,14 +22,13 @@ class App
 
   def option_selected(number)
     case number
-   
-
     when 2
       MusicAlbum.list_all_music_albums(music_albums)
+    when 4
+      Genre.list_all_genres(genre)
     when 8
       MusicAlbum.add_music_album(music_albums, genre, authors)
     when 10
-     when 16
       goodbye
       exit
     else
@@ -36,3 +36,4 @@ class App
     end
   end
 end
+
