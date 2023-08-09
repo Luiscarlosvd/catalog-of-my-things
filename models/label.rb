e6fe950e9dddbfb3e8f750bddde92dee6768c23e
Sergio Peralta <seperalta88@gmail.com>
2023-08-09 14:48:43 -0300

    Add list_all_labels method

--- models/label.rb
+++ models/label.rb
@@ -3,10 +3,23 @@ require 'securerandom'
 class Label
   attr_accessor :id, :title, :color, :items
 
-  def initialize(title, color, _id = SecureRandom.random_number(1000))
+  def initialize(title, color, id = SecureRandom.random_number(1000))
     @title = title
     @color = color
+    @id = id
     @items = []
+  end
+
+  def self.list_all_labels(labels)
+    if labels.empty?
+      puts 'No Labels found'
+    else
+      puts 'List of all labels: '
+      labels.each_with_index do |label, index|
+        puts "#{index}) (ID: #{label.id}) Label: #{label.title}, Color: #{label.color}"
+      end
+      puts
+    end
   end
 
   def add_item(item)
