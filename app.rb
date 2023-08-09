af93bebf375aed140029543b23678b1c0bd58acf
Luiscarlosvd <lc26cr7@gmail.com>
2023-08-09 14:57:34 -0400

    Add write methods for labels and books to preserve data in json files

--- app.rb
+++ app.rb
@@ -47,22 +47,22 @@ class App
       Author.list_all_authors(authors)
     when 7
       Book.add_books(books, genre, authors, labels)
-      # write_books
-      # write_genres
-      # write_authors
-      # write_labels
+      write_books
+      write_genres
+      write_authors
+      write_labels
     when 8
       MusicAlbum.add_music_album(music_albums, genre, authors, labels)
       write_genres
       write_music_albums
       write_authors
-      # write_labels
+      write_labels
     when 9
       Game.add_games(games, genre, authors, labels)
       write_games
       write_genres
       write_authors
-      # write_labels
+      write_labels
     when 10
       goodbye
       exit
