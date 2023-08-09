42b21e0f9f1484c6320ce14be54ece330bba2cd0
Sergio Peralta <seperalta88@gmail.com>
2023-08-09 14:50:52 -0300

    Update app options

--- app.rb
+++ app.rb
@@ -3,19 +3,24 @@ require_relative 'preserveData/write_data'
 require_relative 'preserveData/load_data'
 require './models/music_album'
 require_relative 'models/game'
+require './models/author'
+require './models/label'
 require './models/genre'
+require './models/book'
 
 class App
   include WriteData
   include MessageOutputs
   include LoadData
-  attr_accessor :music_albums, :genre, :authors, :games
+  attr_accessor :music_albums, :genre, :authors, :games, :labels, :books
 
   def initialize
     @genre = load_genres
     @authors = load_authors
     @music_albums = load_music_albums
     @games = load_games
+    @labels = []
+    @books = []
   end
 
   def run
@@ -28,24 +33,36 @@ class App
 
   def option_selected(number)
     case number
+    when 1
+      Book.list_all_books(books)
     when 2
       MusicAlbum.list_all_music_albums(music_albums)
     when 3
       Game.list_all_games(games)
     when 4
       Genre.list_all_genres(genre)
+    when 5
+      Label.list_all_labels(labels)
     when 6
       Author.list_all_authors(authors)
+    when 7
+      Book.add_books(books, genre, authors, labels)
+      # write_books
+      # write_genres
+      # write_authors
+      # write_labels
     when 8
-      MusicAlbum.add_music_album(music_albums, genre, authors)
+      MusicAlbum.add_music_album(music_albums, genre, authors, labels)
       write_genres
       write_music_albums
       write_authors
+      # write_labels
     when 9
-      Game.add_games(games, genre, authors)
+      Game.add_games(games, genre, authors, labels)
       write_games
       write_genres
       write_authors
+      # write_labels
     when 10
       goodbye
       exit
